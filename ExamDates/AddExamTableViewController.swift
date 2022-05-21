bad28ce52a4e879e77a5f542e9816be491ace890
Muallim <muallim@Muallim-Mac.local>
2022-05-21 07:12:12 -0700

    first commit

---
+++ ExamDates/AddExamTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddExamTableViewController.swift
+//  ExamDates
+//
+//  Created by Muallim on 21.05.2022.
+//
+
+import UIKit
+
+class AddExamTableViewController : UITableViewController {
+    
+}
---
+++ ExamDates/ExamTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ExamTableViewController.swift
+//  ExamDates
+//
+//  Created by Muallim on 21.05.2022.
+//
+
+import UIKit
+
+class ExamTableViewController : UITableViewController {
+    
+}

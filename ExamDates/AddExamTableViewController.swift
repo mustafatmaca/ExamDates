8f3d96979554ab7d2ebf4b9bdfd55a74962f076d
mustafatmaca <mustafaatmaca362@gmail.com>
2022-05-31 09:55:16 -0700

    remainTime added

--- ExamDates/AddExamTableViewController.swift
+++ ExamDates/AddExamTableViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class AddExamTableViewController : UITableViewController {
+class AddExamTableViewController : UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var exam : ExamItem?
     let examManager = ExamManager()
+    var remainTime : String?
+    
+    let source = ["5 Min", "10 Min", "15 Min", "30 Min", "45 Min", "1 Hour", "1 Hour 30 Min", "2 Hour"]
     
     let dateLabelCellIndexPath = IndexPath(row: 1, section: 1)
     let datePickerCellIndexPath = IndexPath(row: 2, section: 1)
+    let remainTimePickerCellIndexPath = IndexPath(row: 3, section: 1)
     
     
     @IBOutlet weak var titleTextField: UITextField!
@@ -21,15 +25,36 @@ class AddExamTableViewController : UITableViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var addBarButton: UIBarButtonItem!
+    @IBOutlet weak var remainTimePicker: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        remainTimePicker.delegate = self
+        remainTimePicker.dataSource = self
         
         var dateComponents = DateComponents()
         dateComponents.setValue(2, for: .minute)
         
         //datePicker'daki minimum tarihi belirler.
         datePicker.minimumDate = Calendar.current.date(byAdding: dateComponents, to: Date())!
+        updateDateViews()
+    }
+    
+    func numberOfComponents(in remainTimePicker: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ remainTimePicker: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return source.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+         return source[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.remainTime = source[row]
         updateDateViews()
     }
     
@@ -56,6 +81,12 @@ class AddExamTableViewController : UITableViewController {
             } else {
                 return 0
             }
+        case remainTimePickerCellIndexPath:
+            if remindMeSwitch.isOn {
+                return 216
+            } else {
+                return 0
+            }
         default:
             return 44
         }
@@ -67,7 +98,41 @@ class AddExamTableViewController : UITableViewController {
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .short
         
-        dateLabel.text = dateFormatter.string(from: datePicker.date)
+        var date : Date?
+        
+        date = datePicker.date
+        switch remainTime {
+        case "5 Min":
+            date?.addTimeInterval(5 * 60)
+        case "10 Min":
+            date?.addTimeInterval(10 * 60)
+        case "15 Min":
+            date?.addTimeInterval(15 * 60)
+        case "30 Min":
+            date?.addTimeInterval(30 * 60)
+        case "45 Min":
+            date?.addTimeInterval(45 * 60)
+        case "1 Hour":
+            date?.addTimeInterval(60 * 60)
+        case "1 Hour 30 Min":
+            date?.addTimeInterval(90 * 60)
+        case "2 Hour":
+            date?.addTimeInterval(120 * 60)
+        default:
+            date?.addTimeInterval(0)
+        }
+        
+        
+        dateLabel.text = dateFormatter.string(from: date!)
+    }
+    
+    func updateCells() {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+
+    @IBAction func remindMeSwitchValueChanged(_ sender: UISwitch) {
+        updateCells()
     }
     
     //datePickerValue değiştiğinde updateDateViews methodunu çağırır.
@@ -86,6 +151,26 @@ class AddExamTableViewController : UITableViewController {
         
         if remindMeSwitch.isOn {
             date = datePicker.date
+            switch remainTime {
+            case "5 Min":
+                date?.addTimeInterval(5 * 60)
+            case "10 Min":
+                date?.addTimeInterval(10 * 60)
+            case "15 Min":
+                date?.addTimeInterval(15 * 60)
+            case "30 Min":
+                date?.addTimeInterval(30 * 60)
+            case "45 Min":
+                date?.addTimeInterval(45 * 60)
+            case "1 Hour":
+                date?.addTimeInterval(60 * 60)
+            case "1 Hour 30 Min":
+                date?.addTimeInterval(90 * 60)
+            case "2 Hour":
+                date?.addTimeInterval(120 * 60)
+            default:
+                date?.addTimeInterval(0)
+            }
         }
         
         let newExam = ExamItem(title: titleTextField.text!, date: date)
